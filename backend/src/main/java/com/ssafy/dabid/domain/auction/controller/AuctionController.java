68a1be39503b8dedf898f15d154b6e96928710bb
유우준 <wj122800@gmail.com>
2024-09-10 17:30:39 +0900

    feat: create s3 bucket setting

--- backend/src/main/java/com/ssafy/dabid/domain/auction/controller/AuctionController.java
+++ backend/src/main/java/com/ssafy/dabid/domain/auction/controller/AuctionController.java
@@ -1,7 +1,9 @@
 package com.ssafy.dabid.domain.auction.controller;
 
 import com.ssafy.dabid.domain.auction.dto.request.RegistrationAuctionDto;
+import com.ssafy.dabid.domain.auction.dto.request.TestImageDto;
 import com.ssafy.dabid.domain.auction.service.AuctionService;
+import com.ssafy.dabid.global.utils.S3Util;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.quartz.SchedulerException;
@@ -9,11 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/auctions")
 @RequiredArgsConstructor
 public class AuctionController {
     private final AuctionService service;
+    private final S3Util s3Util;
 
     @GetMapping("/")
     public ResponseEntity<?> getAuctions(){
@@ -43,5 +48,12 @@ public class AuctionController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @PostMapping("/test-image")
+    public void testImage(@ModelAttribute TestImageDto testImageDto) {
+        List<String> imageList = s3Util.uploadFiles(testImageDto.getImages());
+        for(String image : imageList){
+            System.out.println(image);
+        }
+    }
 
 }
