d3c34df215e1e1f0a4502d6c59a5190e4814187e
taegun1011 <xorjsghkd1011@gmail.com>
2024-09-11 18:10:23 +0900

    Feat: Add DTO for authorization

---
+++ backend/src/main/java/com/ssafy/dabid/domain/member/dto/response/AuthResponseDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.dabid.domain.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponseDto {
+    String access;
+
+    String refresh;
+}
