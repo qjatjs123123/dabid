685b2766fe97475b9ab27517643b38262ebb51eb
홍범선 <qjatjs123123@naver.com>
2024-09-19 11:06:09 +0900

    feat: error 해결

--- backend/src/main/java/com/ssafy/dabid/domain/member/entity/Account.java
+++ backend/src/main/java/com/ssafy/dabid/domain/member/entity/Account.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.ssafy.dabid.domain.member.entity;
 
 import jakarta.persistence.*;
@@ -25,29 +24,4 @@ public class Account {
     private String account_number;
 
 }
-=======
-//package com.ssafy.dabid.domain.member.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//@Entity
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Getter
-//@Setter
-//@Builder
-//@Table(name = "member_account")
-//public class Account {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int id;
-//
-//    @OneToOne(fetch = FetchType.LAZY)
-//    private Member member;
-//
-//    @Column
-//    private String account_number;
-//
-//}
->>>>>>> 6dfb07e22d331e686c7a35fda1123b2b80f551e6
+
