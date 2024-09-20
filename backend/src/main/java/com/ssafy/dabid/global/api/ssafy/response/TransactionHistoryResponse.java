dd9546a7e117fb56067ba8e1048edf292e6940c0
장윤주 <umulumul169@gmail.com>
2024-09-20 16:33:33 +0900

    Chores: remove unused classes

--- backend/src/main/java/com/ssafy/dabid/global/api/ssafy/response/TransactionHistoryResponse.java
+++ backend/src/main/java/com/ssafy/dabid/global/api/ssafy/response/TransactionHistoryResponse.java
@@ -1,33 +1,14 @@
-//package com.ssafy.dabid.global.api.ssafy.response;
-//
-//import com.fasterxml.jackson.annotation.JsonInclude;
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import com.ssafy.dabid.domain.deal.dto.response.SsafyApiResponse;
-//import lombok.*;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//@JsonInclude(JsonInclude.Include.NON_NULL)
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class TransactionHistoryResponse extends SsafyApiResponse {
-//    @JsonProperty("REC")
-//    private TransactionHistoryResponseList rec;
-//}
-
 package com.ssafy.dabid.global.api.ssafy.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafy.dabid.domain.deal.dto.response.SsafyApiHeaderResponse;
-import com.ssafy.dabid.domain.deal.dto.response.SsafyApiResponse;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
-import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
