72f82504bebda55d23f2cf3d214cf65c974c8481
taegun1011 <xorjsghkd1011@gmail.com>
2024-09-24 12:17:37 +0900

    Feat: include coolSMS JDK

---
+++ backend/src/main/java/com/ssafy/dabid/global/config/CoolSMSConfig.java
@@ -0,0 +1,19 @@
+package com.ssafy.dabid.global.config;
+
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CoolSMSConfig {
+
+    private static final String API_KEY = "NCSVM9LZZQ6YP3X5";
+    private static final String API_SECRET_KEY = "8WRPICX5BCTVFHIORH0ECM0KPXRYZGFO";
+    private static final String SENDER = "01066406343";
+
+    @Bean
+    public DefaultMessageService messageService(){
+        return NurigoApp.INSTANCE.initialize(API_KEY, API_SECRET_KEY, "https://api.coolsms.co.kr");
+    }
+}
