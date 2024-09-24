067181d1d089ac51c9226811268a00dd1149ab95
taegun1011 <xorjsghkd1011@gmail.com>
2024-09-24 10:37:41 +0900

    Feat: Implement excel download

--- backend/src/main/java/com/ssafy/dabid/domain/inquiry/service/InquiryService.java
+++ backend/src/main/java/com/ssafy/dabid/domain/inquiry/service/InquiryService.java
@@ -2,6 +2,7 @@ package com.ssafy.dabid.domain.inquiry.service;
 
 import com.ssafy.dabid.domain.inquiry.dto.InquiryRequestDto;
 import com.ssafy.dabid.global.status.CommonResponseDto;
+import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.IOException;
 
@@ -9,4 +10,5 @@ public interface InquiryService {
     CommonResponseDto createInquiry(InquiryRequestDto dto);
     CommonResponseDto getMyInquiry();
     String writeCsv() throws IOException;
+    Workbook createExcelFile();
 }
