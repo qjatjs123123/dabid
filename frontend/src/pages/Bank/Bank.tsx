f7547279f2d38a6aab41e5c0be168a24ce38ab6d
taegun1011 <xorjsghkd1011@gmail.com>
2024-09-30 15:27:07 +0900

    Design: update bank modal css

--- frontend/src/pages/Bank/Bank.tsx
+++ frontend/src/pages/Bank/Bank.tsx
@@ -1,5 +1,6 @@
 // src/components/Modal.js
 import React, { useEffect, useState } from 'react';
+import { getImgUrl } from '../../util/Functions';
 
 interface ModalProps {
   isOpen: boolean;
@@ -25,25 +26,47 @@ const BankModal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
       className={`fixed inset-0 flex items-end justify-end transition-opacity duration-300 ${isOpen ? 'opacity-100' : 'opacity-0'}`}
     >
       <div
-        className={`bg-white p-6 rounded-lg shadow-lg max-w-md w-full mb-10 transform transition-transform duration-300 ${isOpen ? 'translate-y-0' : 'translate-y-10'}`}
+        className={`bg-gray-100 h-2/3 rounded-3xl shadow-lg max-w-lg w-full mb-10 transform transition-transform duration-300 ${isOpen ? 'translate-y-0' : 'translate-y-10'}`}
       >
-        <h2 className="text-xl font-bold mb-4">다비드뱅크에 오신 것을 환영합니다.</h2>
-        <p className="text-2xl font-semibold">123-4567890-123</p>
-        <p className="text-2xl font-semibold">10,000,000 원</p>
-
-        <h3 className="mt-6 font-bold">최근 거래 내역</h3>
-        <div className="mt-2">
-          <p className="text-gray-600">09/03</p>
-          <p className="text-green-500">+ 30,000 다비드 포인트 환전 13:01</p>
-          <p className="text-red-500">- 30,000 다비드 낙찰금 결제 13:01</p>
-          <p className="text-gray-600">09/01</p>
-          <p className="text-red-500">- 30,000 다비드 포인트 결제 13:01</p>
-          <p className="text-green-500">+ 1 다비드 계좌 인증 0123 13:01</p>
+        <div className={`bg-white h-20 rounded-3xl flex flex-col justify-center`}>
+          <div className="flex items-center">
+            <img src={getImgUrl('floating/bank.png')} alt="다비드뱅크" className="m-4 mr-0" />
+            <h2 className="text-2xl px-4">다비드뱅크에 오신 것을 환영합니다.</h2>
+            <button
+              onClick={onClose}
+              className="absolute text-3xl top-4 right-4 text-gray-600 hover:text-gray-900"
+              aria-label="닫기"
+            >
+              &times;
+            </button>
+          </div>
         </div>
+        <div className={'p-6'}>
+          <div className={`bg-white flex flex-col h-36 justify-evenly px-6 rounded-3xl`}>
+            <div>
+              <p className="text-4xl font-semibold">
+                <i>123-4567890-123</i>
+              </p>
+            </div>
+            <div>
+              <p className="text-3xl font-semibold">
+                <i>10,000,000 </i>원
+              </p>
+            </div>
+          </div>
 
-        <button onClick={onClose} className="mt-4 bg-blue-500 text-white p-2 rounded">
-          닫기
-        </button>
+          <div className={`bg-white h-72 mt-6 py-1 px-4 rounded-3xl`}>
+            <h3 className="mt-6 text-2xl font-bold">최근 거래 내역</h3>
+            <div className="mt-2">
+              <p className="text-gray-600">09/03</p>
+              <p className="text-green-500">+ 30,000 다비드 포인트 환전 13:01</p>
+              <p className="text-red-500">- 30,000 다비드 낙찰금 결제 13:01</p>
+              <p className="text-gray-600">09/01</p>
+              <p className="text-red-500">- 30,000 다비드 포인트 결제 13:01</p>
+              <p className="text-green-500">+ 1 다비드 계좌 인증 0123 13:01</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Chatbot/Chatbot.tsx
+++ frontend/src/pages/Chatbot/Chatbot.tsx
@@ -27,6 +27,13 @@ const ChatbotModal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
       <div
         className={`bg-white p-6 rounded-lg shadow-lg max-w-md w-full mb-10 transition-transform duration-300 transform ${isOpen ? 'translate-y-0 scale-100' : 'translate-y-10 scale-95'}`}
       >
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-gray-600 hover:text-gray-900"
+          aria-label="닫기"
+        >
+          &times;
+        </button>
         <h2 className="text-xl font-bold mb-4">BidMe: 다비드 AI 챗봇</h2>
         <div className="mb-4">
           <p className="bg-gray-200 p-2 rounded-lg">안녕, 나는 비드미! 다비드에 대해 궁금한 게 있으면 물어봐!</p>
@@ -40,18 +47,18 @@ const ChatbotModal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
           <button className="text-blue-500 underline">고객센터 문의하기 &gt;&gt;</button>
         </div>
         <div className="flex flex-wrap gap-2 mt-4">
-          <p className="bg-gray-100 p-2 rounded-lg">이름이 왜 비드미야?</p>
-          <p className="bg-gray-100 p-2 rounded-lg">입찰을 취소하고 싶어.</p>
-          <p className="bg-gray-100 p-2 rounded-lg">낙찰되지 않은 경매는 어떻게 돼?</p>
-          <p className="bg-gray-100 p-2 rounded-lg">보증금은 언제 환급받을 수 있어?</p>
-          <p className="bg-gray-100 p-2 rounded-lg">다비드는 어떤 서비스야?</p>
-          <p className="bg-gray-100 p-2 rounded-lg">경매는 어떤 방식으로 진행해?</p>
-          <p className="bg-gray-100 p-2 rounded-lg">고객센터</p>
+          <button className="bg-gray-100 p-2 rounded-lg">이름이 왜 비드미야?</button>
+          <button className="bg-gray-100 p-2 rounded-lg">입찰을 취소하고 싶어.</button>
+          <button className="bg-gray-100 p-2 rounded-lg">낙찰되지 않은 경매는 어떻게 돼?</button>
+          <button className="bg-gray-100 p-2 rounded-lg">보증금은 언제 환급받을 수 있어?</button>
+          <button className="bg-gray-100 p-2 rounded-lg">다비드는 어떤 서비스야?</button>
+          <button className="bg-gray-100 p-2 rounded-lg">경매는 어떤 방식으로 진행해?</button>
+          <button className="bg-gray-100 p-2 rounded-lg">고객센터</button>
         </div>
-        <p className="text-gray-600 mt-4">다비드에 대해 궁금한 점은 무엇이든 물어보세요!</p>
-        <button onClick={onClose} className="mt-4 bg-blue-500 text-white p-2 rounded">
-          닫기
-        </button>
+        <div className="flex">
+          <input className="text-gray-600 mt-4" placeholder="다비드에 대해 궁금한 점은 무엇이든 물어보세요!"></input>
+          <button></button>
+        </div>
       </div>
     </div>
   );
