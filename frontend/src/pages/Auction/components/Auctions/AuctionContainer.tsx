987049d012eddabe4022e914a679d4c12e371b04
유우준 <wj122800@gmail.com>
2024-10-02 10:28:35 +0900

    feat: AuctionInput API added

--- frontend/src/pages/Auction/components/Auctions/AuctionContainer.tsx
+++ frontend/src/pages/Auction/components/Auctions/AuctionContainer.tsx
@@ -40,9 +40,9 @@ const AuctionContainer = () => {
       try {
         // 로컬 스토리지에서 accessToken 가져오기
         const accessToken =
-          'eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImZyb250X21hc3RlckBzc2FmeS5jb20iLCJpYXQiOjE3Mjc2NzA0MTQsImV4cCI6MTcyNzY4MTIxNH0.4eigcgmjsekKsywFRKt5cblDXPrSvkH4PdzlTy1Gnu4'; //localStorage.getItem('accessToken');
+          'eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImE1MDVhZHNhc2Q1c3NhZnkwMkBzc2FmeS5jb20iLCJpYXQiOjE3Mjc2NzUyMjYsImV4cCI6MTcyNzY4NjAyNn0.O6MiW2E9XhUGgoDhiHS0tO0oLMt1GXDmudiUy9Ja1BE'; //localStorage.getItem('accessToken');
 
-        const response = await fetch('https://j11a505.p.ssafy.io/api/auctions', {
+        const response = await fetch('http://localhost:4040/api/auctions', {
           method: 'GET', // 기본값은 GET이지만 명시적으로 추가
           headers: {
             'Content-Type': 'application/json',
