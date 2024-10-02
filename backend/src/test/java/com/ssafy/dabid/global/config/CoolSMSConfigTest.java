2952f96ca53524c7c3e4bc73c647899289f278c7
minjoung0304 <minjoung0304@naver.com>
2024-10-02 13:34:35 +0900

    fix:test주석

--- backend/src/test/java/com/ssafy/dabid/global/config/CoolSMSConfigTest.java
+++ backend/src/test/java/com/ssafy/dabid/global/config/CoolSMSConfigTest.java
@@ -11,29 +11,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-class CoolSMSConfigTest {
-
-    @Autowired
-    DefaultMessageService messageService;
-
-    @Test
-    void 동작여부확인() {
-        MessageListRequest request = new MessageListRequest();
-        request.setLimit(1);
-
-        MessageListResponse response = messageService.getMessageList(request);
-        System.out.println(response);
-    }
-
-    @Test
-    void 이게되겠어(){
-        Message message = new Message();
-
-        message.setFrom("01066406343");
-        message.setTo("01045649350");
-        message.setText("이게되겠냐고");
-
-        messageService.sendOne(new SingleMessageSendingRequest(message));
-    }
-}
+//@SpringBootTest
+//class CoolSMSConfigTest {
+//
+//    @Autowired
+//    DefaultMessageService messageService;
+//
+//    @Test
+//    void 동작여부확인() {
+//        MessageListRequest request = new MessageListRequest();
+//        request.setLimit(1);
+//
+//        MessageListResponse response = messageService.getMessageList(request);
+//        System.out.println(response);
+//    }
+//
+//    @Test
+//    void 이게되겠어(){
+//        Message message = new Message();
+//
+//        message.setFrom("01066406343");
+//        message.setTo("01045649350");
+//        message.setText("이게되겠냐고");
+//
+//        messageService.sendOne(new SingleMessageSendingRequest(message));
+//    }
+//}
--- backend/src/test/java/com/ssafy/dabid/utils/JwtUtilsTest.java
+++ backend/src/test/java/com/ssafy/dabid/utils/JwtUtilsTest.java
@@ -8,18 +8,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-class JwtUtilsTest {
-    @Autowired
-    JwtUtils jwtUtils;
-
-    @Test
-    void generatedTokenIsCorrect() {
-        String email = "xorjsghkd1011@gmail.com";
-
-        String accessToken = jwtUtils.createToken(email, TokenType.ACCESS);
-        String emailFromToken = jwtUtils.extractTokenIfValid(accessToken, TokenType.ACCESS);
-
-        assertTrue(email.equals(emailFromToken));
-    }
-}
+//@SpringBootTest
+//class JwtUtilsTest {
+//    @Autowired
+//    JwtUtils jwtUtils;
+//
+//    @Test
+//    void generatedTokenIsCorrect() {
+//        String email = "xorjsghkd1011@gmail.com";
+//
+//        String accessToken = jwtUtils.createToken(email, TokenType.ACCESS);
+//        String emailFromToken = jwtUtils.extractTokenIfValid(accessToken, TokenType.ACCESS);
+//
+//        assertTrue(email.equals(emailFromToken));
+//    }
+//}
