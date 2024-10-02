b17560687639c1ea788838c60212e956e6df2a80
josungyuk <josungyuk@gmail.com>
2024-10-02 17:38:58 +0900

    fix: AuctionList

--- frontend/src/pages/Auction/components/Auctions/AuctionContainer.tsx
+++ frontend/src/pages/Auction/components/Auctions/AuctionContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export class AuctionListDto {
@@ -31,10 +31,40 @@ export class AuctionListDto {
 
 interface AuctionContainerProps {
   auctionList: AuctionListDto[]; // props로 auctionList를 받음
+  setAuctionList: React.Dispatch<React.SetStateAction<AuctionListDto[]>>; // 상태 업데이트 함수
 }
 
-const AuctionContainer: React.FC<AuctionContainerProps> = ({ auctionList }) => {
+const AuctionContainer: React.FC<AuctionContainerProps> = ({ auctionList, setAuctionList }) => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchAuctions = async () => {
+      try {
+        const accessToken = localStorage.getItem('accessToken');
+
+        const response = await fetch('https://j11a505.p.ssafy.io/api/auctions', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('네트워크 응답이 좋지 않습니다.');
+        }
+
+        const data: AuctionListDto[] = await response.json();
+        setAuctionList(data); // API로부터 받은 데이터를 상태로 업데이트
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error.message);
+        }
+      }
+    };
+
+    fetchAuctions();
+  }, [setAuctionList]); // setAuctionList가 변경될 때마다 호출
 
   const handleAuctionClick = (auctionId: string) => {
     navigate(`/auctions/${auctionId}`);
