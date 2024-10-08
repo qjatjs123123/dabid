a5239b73060c83de989fb9df95f1aef6ab483393
홍범선 <qjatjs123123@naver.com>
2024-10-08 18:01:19 +0900

    feat:문의사항 수정

--- frontend/src/pages/Inquiry/Inquiry.tsx
+++ frontend/src/pages/Inquiry/Inquiry.tsx
@@ -7,12 +7,12 @@ import axios from '../../api/axiosConfig';
 import { INQUIRY_API_URL } from '../../util/Constants';
 
 const Inquiry = () => {
-  const [activePage, setActivePage] = useState<string>('문의 목록');
+  const [activePage, setActivePage] = useState<string>('내 문의');
   const [userInfo] = useRecoilState<UserInfo | null>(userState);
 
   const renderContent = () => {
     switch (activePage) {
-      case '문의 목록':
+      case '내 문의':
         return <InquiryList />;
       case '문의 작성':
         return <InquiryInput />;
@@ -34,24 +34,35 @@ const Inquiry = () => {
   };
 
   return (
-    <div className="container w-full border-gray-300 flex flex-col mt-4 p-6">
-      {userInfo?.role !== 'ADMIN' && activePage === '문의 목록' && (
-        <button onClick={() => setActivePage('문의 작성')} className="text-left w-[110px]">
-          ▶ 문의 작성
-        </button>
-      )}
-      {userInfo?.role !== 'ADMIN' && activePage === '문의 작성' && (
-        <button onClick={() => setActivePage('문의 목록')} className="text-left w-[110px]">
-          ▶ 문의 목록
-        </button>
-      )}
-      {userInfo?.role === 'ADMIN' && (
-        <button onClick={saveInquiry} className="text-left w-[130px]">
-          ▶ 문의 파일 저장
-        </button>
-      )}
-      <div className="m-1">{renderContent()}</div>
-    </div>
+    <>
+      <div className="w-full lg:h-[45vh] sm:h-auto bg-[#FFE2D2] px-[15%] flex flex-col lg:flex-row justify-center items-center">
+        <div className="flex flex-col mb-8 lg:mb-0 lg:mr-[100px] text-center lg:text-left">
+          <div className="text-3xl lg:text-4xl font-bold mb-[15px]">모두가 안전한</div>
+          <div className="text-3xl lg:text-4xl font-bold mb-[15px]">고객센터</div>
+          <div className="text-[16px] lg:text-[20px] font-normal mb-[10px]">신고, 문의를 해결해주는 서비스를</div>
+          <div className="text-[16px] lg:text-[20px]">지금 경험해보세요.</div>
+        </div>
+        <img src="/center.png" className="h-[25vh] lg:h-[30vh] w-auto"></img>
+      </div>
+      <div className="container w-full border-gray-300 flex flex-col mt-4 p-6 ">
+        <ul className="flex border-gray-300 mb-[10px]">
+          {['내 문의', '문의 작성', ...(userInfo?.role === 'ADMIN' ? ['문의 파일 저장'] : [])].map((page) => {
+            return (
+              <li key={page}>
+                <button
+                  className={`block text-black p-3 ${activePage === page ? 'bg-gray-200' : ' '} hover:font-bold`}
+                  onClick={() => setActivePage(page)}
+                >
+                  {page}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+
+        <div className="m-1">{renderContent()}</div>
+      </div>
+    </>
   );
 };
 
